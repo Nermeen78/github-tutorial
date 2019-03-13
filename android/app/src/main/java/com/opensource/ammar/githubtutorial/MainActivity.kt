7008ae2c3bef68764f7b432f7077f6217244af40
Ammar Atef <a_atef45@yahoo.com>
2019-03-13 15:41:21 +0200

    added puzzles

--- android/app/src/main/java/com/opensource/ammar/githubtutorial/MainActivity.kt
+++ android/app/src/main/java/com/opensource/ammar/githubtutorial/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        // @todo #1 add link to github repo
+        // @todo #1 create a webview and show the blog inside it
     }
 }
