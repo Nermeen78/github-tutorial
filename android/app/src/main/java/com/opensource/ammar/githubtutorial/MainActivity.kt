38b41c102da5b79f1b3e811913b734bdb75386b6
nermeen_saleh <nermeen_saleh@teacomputers.com>
2019-03-14 08:54:59 +0200

    issue1

--- android/app/src/main/java/com/opensource/ammar/githubtutorial/MainActivity.kt
+++ android/app/src/main/java/com/opensource/ammar/githubtutorial/MainActivity.kt
@@ -2,6 +2,10 @@ package com.opensource.ammar.githubtutorial
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.content.Intent
+import android.net.Uri
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +14,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // @todo #1 add link to github repo
         // @todo #1 create a webview and show the blog inside it
+    }
+    fun ShowonGithub(view: View)
+    {
+        val url = "https://github.com/ammaratef45/github-tutorial"
+
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        startActivity(intent)
     }
 }
